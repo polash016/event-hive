670524cd693e1ec2af699b4069b557d313a731e8
Md Robin Sarkar <robin.shrkr@gmail.com>
2024-09-18 00:14:03 +0600

    server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { Server } from 'http'
-// import app from './app'
-// import config from './app/config'
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Server } from 'http'
+import app from './app'
+import config from './app/config'
 
-// async function main() {
-//   const server: Server = app.listen(config.port, () => {
-//     console.log(`Server running at http://localhost:${config.port}`)
-//   })
-// }
+async function main() {
+  const server: Server = app.listen(config.port, () => {
+    console.log(`Server running at http://localhost:${config.port}`)
+  })
+}
 
-// main()
+main()
